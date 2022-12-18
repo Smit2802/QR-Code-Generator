678ed5ecd3d510cf3efa924ca57d0724f4a70cb6
Smit2802 <smitp2802@gmail.com>
2022-12-18 18:36:00 +0530

    nothing changed

--- js/index.js
+++ js/index.js
@@ -1,64 +1,64 @@
-const form=document.getElementById('generate-form');
-const qr=document.getElementById('qrcode');
+const form = document.getElementById('generate-form');
+const qr = document.getElementById('qrcode');
 
-const onGenerateSubmit=(e)=>{
+const onGenerateSubmit = (e) => {
     e.preventDefault();
     clearUI();
 
-    const url=document.getElementById('url').value;
-    const size=document.getElementById('size').value;
+    const url = document.getElementById('url').value;
+    const size = document.getElementById('size').value;
 
-    if(url===''){
+    if (url === '') {
         alert('Please enter a valid URL');
         return;
     }
-    else{
+    else {
         showSpinner();
         setTimeout(() => {
             hideSpinner();
-            generateQRCode(url,size);
+            generateQRCode(url, size);
 
             setTimeout(() => {
-                const saveUrl=qr.querySelector('img').src;
+                const saveUrl = qr.querySelector('img').src;
                 createSaveBtn(saveUrl);
 
-            },50);
+            }, 50);
 
         }, 1000);
 
     }
 };
 
-const generateQRCode=(url,size)=>{
-     const qrcode=new  QRCode('qrcode',{
-        text:url,
-        width:size,
-        height:size,
-     });
+const generateQRCode = (url, size) => {
+    const qrcode = new QRCode('qrcode', {
+        text: url,
+        width: size,
+        height: size,
+    });
 }
 
-const showSpinner=()=>{
-    document.getElementById('spinner').style.display='block';
+const showSpinner = () => {
+    document.getElementById('spinner').style.display = 'block';
 }
 
-const hideSpinner=()=>{
-    document.getElementById('spinner').style.display='none';
+const hideSpinner = () => {
+    document.getElementById('spinner').style.display = 'none';
 }
-const clearUI=()=>{
-    qr.innerHTML='';
-    const saveLink=document.getElementById('save-link');
-    if(saveLink){
+const clearUI = () => {
+    qr.innerHTML = '';
+    const saveLink = document.getElementById('save-link');
+    if (saveLink) {
         saveLink.remove();
     }
 };
 
-const createSaveBtn=(saveUrl)=>{
-    const link=document.createElement('a');
-    link.id='save-link';
-    link.classList='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
-    link.href=saveUrl;
-    link.download='qrcode';
-    link.innerHTML='Save QR Code';
+const createSaveBtn = (saveUrl) => {
+    const link = document.createElement('a');
+    link.id = 'save-link';
+    link.classList = 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+    link.href = saveUrl;
+    link.download = 'qrcode';
+    link.innerHTML = 'Save QR Code';
     document.getElementById('generate').appendChild(link);
 
 };
@@ -67,4 +67,4 @@ const createSaveBtn=(saveUrl)=>{
 
 hideSpinner();
 
-form.addEventListener('submit',onGenerateSubmit);
+form.addEventListener('submit', onGenerateSubmit);
